3f3fd9854081b6ae8d92c12ebfc60b434176fd34
Carlos Nava <machinery.e@gmail.com>
2023-05-24 15:27:20 -0400

    Maquetar compoentes del home

---
+++ src/app/features/features.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FeaturesRoutingModule } from './features-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    FeaturesRoutingModule
+  ]
+})
+export class FeaturesModule { }
